ce8d78e6b39292238e6e0cfdc10c68d4f73a4428
ihnat13 <ignatfomich7@gmail.com>
2024-02-01 20:04:20 +0200

    hw18

--- HW17.js
+++ HW17.js
@@ -1,5 +1,7 @@
 
-const rexegp = /^\s*[+|-]?0*[1-9]\d{5}\s*$/m
+const rexegp = /^\s*[+-]?0*[1-9]\d{5}\s*$/m
+console.log(rexegp.test('|000123456  ')) 
+console.log(rexegp.test('|000123456\n123\nOLOLO  ')) 
 console.log(rexegp.test('   123456  '))              ////-> так
 console.log(rexegp.test(' -0123456  '))               //-> так
 console.log(rexegp.test('     -12345  '))                 //-> ні
